e8761a8182d7bfda6d4b59793fddc5c104cad78c
bluerhythmz <73447805+bluerhythmz@users.noreply.github.com>
2020-12-11 18:41:26 -0800

    --

--- app.js
+++ app.js
@@ -5,13 +5,15 @@ const cancel = document.querySelector('.cancel')
 const playPause = document.querySelector('.icon-music-play-button')
 const skipButton = document.querySelector('.icon-music-end-button')
 const songState = document.getElementById('songState')
+const htmlAudio = document.getElementById('audio')
+htmlAudio.innerHTML = '<audio autoplay><source src="brsongs/Akatsuki No Yona Hip Hop Remix.mp3"></audio>';
 
 let songs = ["brsongs/Akatsuki No Yona Hip Hop Remix.mp3", "brsongs/Blue Rhythmz - Naruto Bump.mp3", "brsongs/Final Fantasy 13 Battle Theme Remix.mp3", "brsongs/Inuyasha Kikyo Theme Remix.mp3",  "brsongs/Inuyasha Shinjitsu no Uta Remix.mp3", "brsongs/Marvel vs Capcom 2 Abyss Theme Remix.mp3", "brsongs/Marvel vs Capcom 2 Clock Tower House Remix.mp3", "brsongs/Ridge Racer Type 4 Garage Talk Remix.mp3", "brsongs/Sonic Unleashed Apotos Remix.mp3", "brsongs/Street Fighter 3 Gill Theme Remix.mp3", "brsongs/Street Fighter 3 Ibuki Hip Hop Remix.mp3", "brsongs/Street Fighter 3 Third Strike Akuma Remix.mp3", "brsongs/Street Fighter 3 Third Strike Makoto Remix.mp3", "brsongs/Street Fighter 3 Third Strike Necro Remix.mp3"]
 let song = new Audio()
-song.crossOrigin = 'anonymous';
+
 let currentSong = Math.floor(Math.random() * songs.length)
 
-window.onload = pauseSong()
+/* window.onload = pauseSong() */
 
 function playSong() {
     song.src = songs[currentSong]
